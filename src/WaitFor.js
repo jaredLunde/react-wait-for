9db8ac837e66efa0aeb3c58bd503ef83ef484998
Jared Lunde <jared.lunde@gmail.com>
2018-08-20 23:50:41 -0600

    Initial commit

---
+++ src/WaitFor.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Promise from 'cancelable-promise'
+
+
+export default class WaitFor extends React.Component {
+  static propTypes = {
+    img: PropTypes.string,
+    promise: PropTypes.any,
+    loading: PropTypes.element
+  }
+
+  static defaultProps = {
+    loading: 'Loading...'
+  }
+
+  state = {ready: false}
+
+  constructor (props) {
+    super(props)
+    this.promise = props.promise
+
+    if (typeof document !== 'undefined' && props.img) {
+      const img = new Image()
+      img.src = props.img
+
+      this.promise = new Promise(
+        (resolve, reject) => {
+          if (img.complete) {
+            resolve()
+          }
+          else {
+            img.onload = resolve
+          }
+        }
+      )
+    }
+  }
+
+  componentDidMount () {
+    if (this.promise !== null && this.promise !== void 0) {
+      this.promise.then(() => this.setState({ready: true}))
+    }
+  }
+
+  componentWillUnmount () {
+    if (this.promise && this.promise.cancel !== void 0) {
+      this.promise.cancel()
+    }
+  }
+
+  render () {
+    return this.state.ready ? this.props.children : this.props.loading
+  }
+}
